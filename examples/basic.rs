9b0681ef3dcd126692c1766c0740dceb7bdcc9c4
Ben Anderson <gravityscore@gmail.com>
2015-12-15 14:37:33 +0800

    Initial commit

---
+++ examples/basic.rs
@@ -0,0 +1,19 @@
+
+//
+//  Basic Example
+//
+
+extern crate ncurses;
+extern crate nterm;
+
+use nterm::{Terminal, Drawable, Attributes, Cursor, Color};
+
+
+fn main() {
+	let term = Terminal::new();
+	term.cursor_state(Cursor::Invisible);
+	let mut attrs = Attributes::colors(Color::Red, Color::Normal);
+	attrs.underline = true;
+	term.write("hello!", 0, 0, attrs);
+	term.poll_event();
+}
